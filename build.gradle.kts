fd4ffd56392cf9ae9fed74de14d927efe9bb6aa0
lokspel <208148594+lokspel@users.noreply.github.com>
2025-12-11 23:02:28 +0400

    PacketEvents was missing huh

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,25 +32,34 @@ repositories {
         name = "placeholderapi"
         url = uri("https://repo.extendedclip.com/content/repositories/placeholderapi/")
     }
-}
+    maven {
+        name = "codemc-releases"
+        url = uri("https://repo.codemc.io/repository/maven-releases/")
+    }
+    maven {
+        name = "codemc-snapshots"
+        url = uri("https://repo.codemc.io/repository/maven-snapshots/")
+    }
 
-dependencies {
-    compileOnly("com.hpfxd.pandaspigot:pandaspigot-api:1.8.8-R0.1-SNAPSHOT")
-    compileOnly("org.projectlombok:lombok:1.18.34")
-    annotationProcessor("org.projectlombok:lombok:1.18.34")
-    compileOnly("net.dmulloy2:ProtocolLib:5.4.0")
-    compileOnly("me.clip:placeholderapi:2.11.6")
+    dependencies {
+        compileOnly("com.hpfxd.pandaspigot:pandaspigot-api:1.8.8-R0.1-SNAPSHOT")
+        compileOnly("org.projectlombok:lombok:1.18.34")
+        annotationProcessor("org.projectlombok:lombok:1.18.34")
+        compileOnly("net.dmulloy2:ProtocolLib:5.4.0")
+        compileOnly("me.clip:placeholderapi:2.11.6")
+        compileOnly("com.github.retrooper:packetevents-spigot:2.11.0")
 
-    implementation("org.bstats:bstats-bukkit:3.0.2")
-}
+        implementation("org.bstats:bstats-bukkit:3.0.2")
+    }
 
-tasks.processResources {
-    filteringCharset = "UTF-8"
-}
+    tasks.processResources {
+        filteringCharset = "UTF-8"
+    }
 
-tasks.shadowJar {
-    archiveBaseName.set("ZonePractice Lite")
-    archiveClassifier.set("")
+    tasks.shadowJar {
+        archiveBaseName.set("ZonePractice Lite")
+        archiveClassifier.set("")
 
-    relocate("org.bstats", "dev.nandi0813.practice.dependencies")
+        relocate("org.bstats", "dev.nandi0813.practice.dependencies")
+    }
 }
